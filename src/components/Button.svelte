ac3386efcc9c1a8791329956f102c34aef1641e9
AmeKishibe <emma.guillaume@mmibordeaux.com>
2023-02-03 15:12:37 +0100

    add style button components

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
 	export let url: string;
 	export let disabled: boolean;
+	console.log(disabled)
 </script>
-
+<div class="{disabled ? "w-48 bg-backgroundDisabled text-textDisabled px-6 py-3 rounded-md" : "w-48 flex justify-center	bg-background text-text px-6 py-3 rounded-md"}">
 {#if !disabled}
-<a href={url} class="text-buttons"><slot /></a>
+<a href={url}><slot /></a>
 {:else}
 	<slot />
 {/if}
+</div>
