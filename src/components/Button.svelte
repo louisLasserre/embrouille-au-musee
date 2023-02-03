f8d27e2c114841981e8fc655767fc6b5328d5715
aureliane dor <aureliane.dor@gmail.com>
2023-02-03 15:29:47 +0100

    fix Button

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -3,9 +3,9 @@
 	export let disabled: boolean;
 
 </script>
-<div class="{disabled ? "w-48 bg-backgroundDisabled text-textDisabled px-6 py-3 rounded-md" : "w-48 flex justify-center	bg-background text-text px-6 py-3 rounded-md"}">
+<div class={disabled ? "w-48 bg-backgroundDisabled text-textDisabled px-6 py-3 rounded-md" : "w-48 flex justify-center	bg-background text-text px-6 py-3 rounded-md"}>
 {#if !disabled}
-<a href={url}><slot /></a>
+	<a href={url}><slot /></a>
 {:else}
 	<slot />
 {/if}
