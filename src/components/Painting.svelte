772200cc7c1490848e364333656f58ad6aae3028
Auréliane DOR <aureliane.dor@gmail.com>
2023-02-07 17:57:03 +0100

    wip: style explore page

--- src/components/Painting.svelte
+++ src/components/Painting.svelte
@@ -3,7 +3,7 @@
 	export let alt: HTMLImageElement['alt'];
 </script>
 
-<div class="relative w-full h-4/5 overflow-hidden">
+<div class="relative w-full h-3/5 overflow-hidden">
 	<img class="absolute" {src} {alt} />
 	<slot />
 </div>
