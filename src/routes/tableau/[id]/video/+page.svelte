efcad654c65614573d3bb95bc245f74769c4503e
louis lasserre <louis.lasserre@mmibordeaux.com>
2023-02-08 09:21:26 +0100

    pop up aide

--- src/routes/tableau/[id]/video/+page.svelte
+++ src/routes/tableau/[id]/video/+page.svelte
@@ -5,6 +5,8 @@
 	import Painting from 'src/components/Painting.svelte';
 
 	import type { PageData } from '../$types';
+	import ButtonIndice from 'src/components/ButtonIndice.svelte';
+	import Modal from 'src/components/Modal.svelte';
 
 	export let data: PageData;
 
@@ -14,6 +16,9 @@
 	const source = [`/videos/madame-samazeuilh.mp4`];
 	const videoHeight = window.innerHeight;
 	const videoWidth = window.innerWidth;
+	let isActive: boolean = false;
+	const modalText =
+		"Cliquez sur le bouton 'explorer l’œuvre' pour observer les peintures. Le but est que vous retrouviez les différences entre les peintures originelles et le tableau affiché sur votre téléphone. ";
 
 	let IsShowMore: boolean = false;
 	const showMore = () => {
@@ -21,7 +26,7 @@
 	};
 </script>
 
-<div class="h-[100vh] relative flex flex-col justify-end">
+<div class="h-[100vh] relative flex flex-col justify-end bg-black">
 	<div class="h-full w-full absolute overflow-hidden">
 		<VideoPlayer
 			{poster}
@@ -38,14 +43,14 @@
 		<div class="gradient" />
 	</div>
 
-	<article class="z-10 relative p-8 pt-0">
+	<article class="z-10  p-8 pt-0">
 		<h1 class="text-background text-titlePainting">{name}</h1>
 		<p class="text-background font-texts font-light ml-4">{description}</p>
 
 		<div class="flex min-w-0 items-start mb-6 {IsShowMore ? 'flex-col' : ''}">
 			<p
-				class="font-textTrans transition-all  overflow-y-auto text-background {!IsShowMore
-					? 'whitespace-nowrap max-h-[2rem]'
+				class="font-textTrans transition-all max-h-[2rem] overflow-y-auto text-background {!IsShowMore
+					? 'whitespace-nowrap '
 					: 'max-h-[6rem]'} overflow-hidden text-ellipsis"
 			>
 				<span class="font-text">Transcription: </span>{transcription}
@@ -54,7 +59,13 @@
 				>Afficher {IsShowMore ? 'moins' : 'plus'}</button
 			>
 		</div>
-		<div class="w-fit mx-auto">
+		<div class="w-fit mx-auto flex gap-6 items-center">
+			<button
+				class="bg-button font-title text-background px-[0.9rem] aspect-square rounded-md text-[22px]"
+				on:click={() => (isActive = true)}>?</button
+			>
+
+			<Modal bind:isActive text={modalText} />
 			<Button disabled={false} url="/tableau/{data.id}/explore">Explorer l'oeuvre</Button>
 		</div>
 	</article>
