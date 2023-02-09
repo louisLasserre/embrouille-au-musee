5fb88f5c657f66bd49b24fe2d62237e98bf94a3a
AmeKishibe <emma.guillaume@mmibordeaux.com>
2023-02-09 14:30:33 +0100

    correction textes

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -7,11 +7,9 @@
 	</h1>
 	<img src="/assets/fond_home.png" class="z-[-1] absolute top-0"/>
 	<p class="px-5 text-textDark text-text font-text leading-5 italic">
-		" Bonjour et bienvenue au <span class="font-button">Musée des Beaux-Arts de Bordeaux</span> !
-		Commençons la visite sans plus tarder ! Mais… Attendez Où est passé le <span class="font-button">testament</span> du tableau <span class="font-button">“Les Héritiers”</span> ?
+		“ Bonjour et bienvenue au <span class="font-button">Musée des Beaux-Arts de Bordeaux</span> ! Commençons la visite sans plus tarder ! Mais… Attendez ! Où est passé le <span class="font-button">testament</span> du tableau <span class="font-button">“Les Héritiers”</span> ?
 		Et que sont devenus le <span class="font-button">chapeau</span> de Madame Samazeuil et le <span class="font-button">foulard</span> d'Alfred Roll !
-		Je ne peux pas commencer la visite dans ses conditions.
-		S’il vous plaît retrouver les <span class="font-button">objets intrus</span> et <span class="font-button">restituez-les</span> au bon endroit ! "
+Je ne peux pas commencer la visite dans ses conditions. S’il vous plaît, retrouvez les <span class="font-button">objets intrus</span> et <span class="font-button">restituez-les</span> au bon endroit ! “
 	</p>
 	<div class="flex justify-center">
 		<img src="/assets/paintings-all.png" />
