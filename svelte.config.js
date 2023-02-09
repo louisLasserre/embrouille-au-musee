503612118dcfd14516b9e3cca3dd52915f7d8d64
aureliane dor <aureliane.dor@gmail.com>
2023-02-09 17:06:02 +0100

    test adaptater

--- svelte.config.js
+++ svelte.config.js
@@ -4,7 +4,6 @@ import adapter from '@sveltejs/adapter-netlify';
 
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
