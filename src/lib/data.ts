33ab9546a3d63c17872ba9f59d9ecfaa8150e438
Auréliane DOR <aureliane.dor@gmail.com>
2023-02-10 16:40:04 +0100

    add remerciement text and fix style home page

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -6,8 +6,9 @@ export interface IPaintingsData {
   clueCollect: string;
   clueReturn: string;
   missingItemId: number;
-  transcription: string
-  reactionVideos: videosUrls[]
+  transcription: string;
+  reactionVideos: videosUrls[];
+  remerciement: string;
 
 }
 export type TVideosName = 'placedItems' | 'clueGetItems' | 'cluePlaceItems';
@@ -24,6 +25,7 @@ export const paintingsData: IPaintingsData[] = [
     itemId: 1 ,
     clueCollect: "Mais que fait ce foulard ici ?!",
     clueReturn: "Je vous en supplie, retrouvez mon testament !",
+    remerciement: "Dieu soit loué mon testament ! Je vous en sais gré !",
     transcription: "Transcription : Ô rage, ô désespoir ! Le testament de notre défunt père a disparu et a été remplacé par ce vieux foulard ! Que dirait notre créateur Eugène Buland ! C’est sûrement cette… cette… nigaude arrogante à l’ego aussi gros que son chapeau, qui nous l’a dérobé, cette madame Samazeuilh ! À quoi rime désormais notre nom les “Héritiers” !? Oh ! Vous êtes là !? Je vous en supplie ! Enlevez-moi ce foulard et retrouvez notre testament !",
     missingItemId: 2,
     reactionVideos: [
@@ -49,6 +51,7 @@ export const paintingsData: IPaintingsData[] = [
     clueCollect: "Que fait cette feuille sur ma tête ?!",
     clueReturn:"Partez à la recherche de mon ravissant chapeau !",
     transcription: "Transcription: Eh vous là ! Oui vous, regardez-moi, ne suis-je pas sublime ? Mon peintre, Guillaume Alaux, a parfaitement su retranscrire ma beauté ! Ce jour-là, je portais mon ravissant chape… Mais … MON CHAPEAUX A DISPARU ! Et puis, que fait ce vieux truc sur ma figure ! C’est ridicule, je ne suis plus écolière ! Je suis sûre que c’est une affaire de ce prétentieux narcissique Alfred Roll, c’est tout à fait son style ! Visiteur, partez diligemment à sa recherche, retirez-moi ce testament de la tête, je ne peux pas sortir en cheveux, c’est indécent !",
+    remerciement: 'Enfin, mon chapeau ! Vous en avez mis du temps !',
     missingItemId: 3,
     reactionVideos: [
       {
@@ -73,6 +76,7 @@ export const paintingsData: IPaintingsData[] = [
     clueCollect: "Ce chapeau hideux ne m’appartient pas !",
     clueReturn:"Dépêchez vous de retrouver mon foulard !",
     transcription: "Transcription: MISÉCORDE ! Tout doit être parfait ! le visiteur ne doit pas voir que mon foulard a disparu… Et ce chapeau, que fait-il sur moi ? Quelle faute de goût ! Sous leurs airs pleurnichards, ne serait-ce pas les Héritiers qui convoitent mon foulard pour en faire un mouchoir ?!  Oh vous qui êtes là ! Hâter vous de redorer mon image ! Et par la même occasion, ôtez-moi cet affreux chapeau !",
+    remerciement: 'Il était temps ! Ma tenue manquait de finesse !',
     missingItemId: 1,
     reactionVideos: [
       {
